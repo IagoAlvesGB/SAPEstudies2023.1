78524e6a91f54e3bb72d87ca070a6692a64ad665
IagoAlvesPersonal <iago.nascimento@grupoboticario.com.br>
2023-07-06 11:23:44 -0300

    Reduce

--- estudo_js/04-lists/for-forin/4.reduce.js
+++ estudo_js/04-lists/for-forin/4.reduce.js
@@ -1,14 +1,24 @@
 const { obterPessoas } = require('./service')
 
+Array.prototype.meuReduce = function (callback, valorInicial){
+    let valorFinal = typeof valorInicial !== undefined ? valorInicial : this[0]
+    for (let index = 0; index <= this.length -1; index++) {
+        const valorFinal = callback(valorFinal, this[index], this)
+    }
+    return valorFinal
+}
+
 async function main(params) {
     try {
         const { results } = await obterPessoas(`a`)
         const pesos = results.map(item => parseInt(item.height))
-        const total = results.reduce((anterior, proximo) => {
+        const total = pesos.reduce((anterior, proximo) => {
             return anterior + proximo
         })
+        console.log(`TOTAL:`, total)
     } catch (error) {
         console.error(`DEU RUIM`, error)
     }
 }
+main()
 
